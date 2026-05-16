aa6291cc822b2a108e701f2e116a5d07f34de895
Aa Kasep <driher@gmail.com>
2026-05-16 11:11:13 +0700

    Perbaikan sub menu akademik

--- app/akademik/panduan/page.tsx
+++ app/akademik/panduan/page.tsx
@@ -5,7 +5,7 @@ export const revalidate = 60;
 
 export default async function Page() {
   const res = await fetch(
-    "https://cms.komunikasi.uinsgd.ac.id/wp-json/wp/v2/pages?slug=staff&_embed",
+    "https://cms.komunikasi.uinsgd.ac.id/wp-json/wp/v2/pages?slug=panduan&_embed",
     { next: { revalidate: 60 } }
   );
 
